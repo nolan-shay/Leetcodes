92e7d6857f45a55653ae6876d13693e5d55481ac
nolan-shay <61945800+nolan-shay@users.noreply.github.com>
2022-10-20 12:46:25 -0500

    Create addOne.java

---
+++ addOne.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int pos = digits.length -1;
+        int remainder = 1;
+        while( pos>=0 && remainder ==1){
+            if (digits[pos] + remainder == 10){
+                digits[pos] = 0;
+            } else{
+                digits[pos] = digits[pos] + remainder;
+                remainder = 0;
+            }
+            pos--;
+        }
+        if ( remainder == 1){
+            int[] ans = new int[digits.length + 1];
+            ans[0] = 1;
+            System.arraycopy(digits, 0, ans, 1, digits.length);
+            return ans;
+        }
+        return digits;
+    }
+}
