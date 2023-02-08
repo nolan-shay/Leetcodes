854802d579eeb1b1ae7aae7c76e4f0b6268230ce
nolan-shay <61945800+nolan-shay@users.noreply.github.com>
2023-02-08 13:37:58 -0600

    Update subtreeOfAnotherTree.java

--- subtreeOfAnotherTree.java
+++ subtreeOfAnotherTree.java
@@ -27,3 +27,24 @@ class Solution {
         return isSameTree(root.left, root2.left) && isSameTree(root.right, root2.right);
     }
 }
+
+
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        //serialize root and subRoot
+        String haystack = serialize(root);
+        String needle = serialize(subRoot);
+        // then we have the classic problem of does string needle exist in string haystack;
+        return haystack.indexOf(needle) != -1 ;
+    }
+
+    public String serialize( TreeNode root){
+        //preorder traversal
+        if (root == null) return "#";
+        StringBuilder answer = new StringBuilder(" " + root.val);
+        answer.append(serialize(root.left) );
+        answer.append(serialize(root.right) );
+        return answer.toString();
+    }
+
+}
