364218417e60e0ae288a76c1a624218a60023811
nolan-shay <61945800+nolan-shay@users.noreply.github.com>
2024-09-05 17:21:08 -0400

    Update sumOfTwoIntegers.java

--- sumOfTwoIntegers.java
+++ sumOfTwoIntegers.java
@@ -89,3 +89,12 @@ class Solution {
 //first thought is convert to binary, add in binary, convert back to int;
 //convert both ints to binary strings
 //
+
+class Solution2 {
+    public int getSum(int a, int b) {
+        if (b == 0){return a;}
+        return (getSum(a^b,(a&b)<<1));
+    }
+
+}
+
