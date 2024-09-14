52e3a83b48b084ea0cff37671866c8ebab24431a
nolan-shay <61945800+nolan-shay@users.noreply.github.com>
2024-09-14 17:13:05 -0400

    Update nonOverlappingIntervals.java

--- nonOverlappingIntervals.java
+++ nonOverlappingIntervals.java
@@ -23,3 +23,39 @@ class Solution {
         
     }
 }
+
+class Solution2 {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> a[1] -b[1]);
+
+        int[] chosen = intervals[0];
+        int count = 0;
+
+        for (int i = 1; i < intervals.length; i++){
+            if ( intervals[i][0] < chosen[1]){
+                count++;
+            } else {
+                chosen = intervals[i];
+            }
+        }
+        return count;
+
+    }
+}
+/*
+sort based on end time
+choose first entry (it ends first)
+then choose the next item in the list that has start time after last scheduled end time
+    that is now current item
+
+sort based on end time
+
+chosen = intervals[0]
+count = 0;
+for ( int i = 1; i< intercals; i++)
+    if intervals[i][0] < chosen[1] //this means that there is interference
+        count ++;
+    else 
+        chosen = intervals[i]
+    */
+
