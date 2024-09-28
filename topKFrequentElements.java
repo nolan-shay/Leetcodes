1b7c81ea91f54654de6215cc3df0dad91d8e5ba7
nolan-shay <61945800+nolan-shay@users.noreply.github.com>
2024-09-28 16:38:45 -0400

    Update topKFrequentElements.java

--- topKFrequentElements.java
+++ topKFrequentElements.java
@@ -38,3 +38,32 @@ iterate over the list, making a freq map element:freq
 
 iterate over the hashset and add all numbers to a priority queue 
 */
+
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for ( int i : nums){
+            if (hm.containsKey(i)){
+                hm.put(i,hm.get(i) + 1);
+            } else {
+                hm.put(i,1);
+            }
+        }
+        List<Integer>[] bucket = new List[nums.length+1];
+        for ( int i = 0; i < nums.length+1; i++){
+            bucket[i] = new ArrayList<>();
+        }
+        for ( Map.Entry<Integer, Integer> el : hm.entrySet()){
+            bucket[el.getValue()].add(el.getKey());
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        int i = nums.length;
+        while ( ans.size() < k){
+            ans.addAll(bucket[i]);
+            i--;
+        }
+        return ans.stream().mapToInt(a -> a).toArray();
+    }
+}
