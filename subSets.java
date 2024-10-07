9ec271e0d40b0082c459b3f8943786c4f381499e
nolan-shay <61945800+nolan-shay@users.noreply.github.com>
2024-10-07 17:29:53 -0400

    Update subSets.java

--- subSets.java
+++ subSets.java
@@ -32,6 +32,29 @@ for ( int i : nums){
     answer.addAll(additionalLists);
     additionalLists = new ArrayList<>();
 }
+
+
 return answer;
 
 */
+
+class Solution {
+    List<List<Integer>> subsets = new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> empty = new ArrayList<>();
+        helper(empty,0,nums);
+        return subsets;
+    }
+
+    public void helper( List<Integer> subset, int pos, int[] nums){
+        if (pos == nums.length){
+            subsets.add(subset);
+            return;
+        }
+        List<Integer> copy = new ArrayList<>(subset);
+        helper(copy,pos+1,nums);
+        subset.add(nums[pos]);
+        helper(subset,pos+1,nums);
+    }
+        
+}
