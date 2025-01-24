3ec72f90c3aa2d751d747dc6125efcea09e97543
nolan-shay <61945800+nolan-shay@users.noreply.github.com>
2025-01-24 08:11:55 -0500

    Update mergeKSortedLists.java

--- mergeKSortedLists.java
+++ mergeKSortedLists.java
@@ -1,3 +1,51 @@
+class SolutionJan25 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode head = null;
+        ListNode cur = null;
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>((a,b)->(a.value-b.value));
+        for(int i = 0; i < lists.length; i++){
+            if (lists[i]!= null){
+                minHeap.add(new Pair(lists[i].val,i));
+            }
+        }
+        if (!minHeap.isEmpty()){
+            Pair smallest = minHeap.poll();
+            // head = cur = new ListNode(smallest.value);
+            head = cur = lists[smallest.index]; //in place
+            lists[smallest.index] = lists[smallest.index].next;
+            if (lists[smallest.index] != null){
+                minHeap.add(new Pair(lists[smallest.index].val, smallest.index));
+            }
+        }
+        while (!minHeap.isEmpty()){
+            Pair smallest = minHeap.poll();
+            // ListNode newNode = new ListNode(smallest.value);
+            ListNode newNode = lists[smallest.index]; // in place
+            cur.next = newNode;
+            cur = newNode;
+            lists[smallest.index] = lists[smallest.index].next;
+            if (lists[smallest.index] != null){
+                minHeap.add(new Pair(lists[smallest.index].val, smallest.index));
+            }
+        }
+
+        return head;
+
+
+    }
+}
+
+ class Pair{
+    public int value;
+    public int index;
+
+    public Pair( int value, int index){
+        this.value = value;
+        this.index = index;
+    }
+}
+
+
 class SolutionPQ {
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists.length == 0) return null;
