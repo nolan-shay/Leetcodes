5454b1139d362d2e44384c251fe233a6ea96fef6
nolan-shay <61945800+nolan-shay@users.noreply.github.com>
2025-02-18 18:16:56 -0500

    Update graphValidTree.java

--- graphValidTree.java
+++ graphValidTree.java
@@ -67,3 +67,47 @@ bfsHelper( int i, edges, visited){
 
 
 */
+
+//2/17/2025 solution
+class Solution2 {
+    boolean isValid;
+    public boolean validTree(int n, int[][] edges) {
+        isValid = true;
+        List<List<Integer>> adj = new ArrayList<>();
+        boolean[] visited = new boolean[n];
+        for (int i =0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for ( int[] edge : edges){
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        dfs(0,-1,adj,visited);
+        for(int i = 0; i < n; i++){
+            if (!visited[i]) return false;
+        }
+        return isValid;
+    }
+
+    public void dfs(int node, int parent, List<List<Integer>> adj, boolean[] visited){
+        if (visited[node]){
+            isValid = false;
+            return;
+        }
+        visited[node] = true;
+        for ( int child : adj.get(node)){
+            if ( child != parent){
+                dfs(child,node, adj,visited);
+            }
+        }
+    }
+}
+
+/**
+make adj list
+perform dfs with visited array
+if we reach a node that has been visited, return false;
+if we perform a dfs and there are still nodes that are unvisited return false
+*/
