e22a366dc3a1e9a2bfa73bc05c23d64e0adf8ff6
nolan-shay <61945800+nolan-shay@users.noreply.github.com>
2025-05-23 07:19:21 -0400

    Update coinChange.java

--- coinChange.java
+++ coinChange.java
@@ -1,3 +1,32 @@
+class Solution { // May 2025
+    public int[][] dpArr;
+    public int[] coins;
+    public int amount;
+    public int coinChange(int[] coins, int amount) {
+        this.coins = coins;
+        this.amount = amount;
+        dpArr = new int[amount+1][coins.length];
+        int take,skip,take2skip;
+        for ( int amt = 1; amt <= amount; amt++){
+            for ( int pos = coins.length-1; pos >=0; pos--){
+                take = 1+getDpArr(amt - coins[pos], pos+1);
+                skip = getDpArr(amt, pos+1);
+                take2skip = 2+getDpArr(amt - coins[pos] - coins[pos], pos);
+                dpArr[amt][pos]= Math.min(take,Math.min(skip,take2skip));
+            }
+        }
+        // System.out.println(Arrays.deepToString(dpArr).replace("], ", "]\n"));
+        return dpArr[amount][0] >= 1000000? -1: dpArr[amount][0];
+    }
+
+    public int getDpArr(int amt, int pos){
+        if (amt == 0) return 0;
+        if ( amt < 0 || amt > amount || pos < 0 || pos >= coins.length) return 1000000;
+        return dpArr[amt][pos];
+    }
+}
+
+
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[][] solutions = new int[coins.length][amount+1];
